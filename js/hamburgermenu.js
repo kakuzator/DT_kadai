ed03a53c5b8e23283a11e57c8267dfe5b14dd739
kakuzator <wyarbo0461@yahoo.co.jp>
2019-09-09 22:28:25 +0900

    レスポンシブipad途中

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,10 +1,13 @@
 $(function () {
 	$('.hamburgermenu').on('click', function () {
 		let openMenu = false;
-		console.log('クリックしたよ');
 		$('header').toggleClass('active')
 		$('.hamburgermenu').toggleClass('active');
 		$('.hidden-menu').toggleClass('open');
 		$('.js-click-menu').toggleClass('active');
+
+		//メニューの高さ決め
+		let wH = $(window).height() - 40; //padding分マイナス
+		$('.js-click-menu').css('height', wH + 'px')
 	})
 })
