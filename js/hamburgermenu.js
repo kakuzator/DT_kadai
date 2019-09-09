1cba9743a4acbe8b090738290650046a143af2a4
kakuzator <wyarbo0461@yahoo.co.jp>
2019-09-09 15:01:31 +0900

    ハンバーガーメニュー仮

--- js/hamburgermenu.js
+++ js/hamburgermenu.js
@@ -1,8 +1,11 @@
 $(function () {
 	$('.hamburgermenu').on('click', function () {
+		let openMenu = false;
 		console.log('クリックしたよ');
+		$('header').toggleClass('active')
 		$('.hamburgermenu').toggleClass('active');
 		$('.hidden-menu').toggleClass('open');
-		$('.js-click-menu').toggleClass('active')
+		$('.js-click-menu').toggleClass('active');
+
 	})
-});
+})
