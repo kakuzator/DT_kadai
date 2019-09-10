729248352a792237bca4c686a027e80a8cbedde0
kakuzator <wyarbo0461@yahoo.co.jp>
2019-09-10 15:34:58 +0900

    ニュース表示

--- js/newsBord.js
+++ js/newsBord.js
@@ -1,5 +1,19 @@
 $(function () {
-	$(window).load("お知らせ用.txt", function (data) {
-		console.log(data);
-	});
+	var news;
+	$.ajax({
+		url: '../js/news.txt',
+		type: 'get',
+		success: function (data) {
+			news = data.split(/\n/);
+			console.log(news);
+			for (i = 0; i < 5; i++) {
+				$('#newsbord').append('<li class="news">' + news[i] + '</li>')
+			}
+			//$('#newsbord').text(news);
+
+		},
+		error: function (data) {
+			alert('データ読み込み失敗')
+		}
+	})
 });
